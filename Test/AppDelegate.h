87512f0546e40752924329111799e850beeb7492
Y W <wangyaweiart@gmail.com>
2018-12-01 10:12:13 +0800

    xxx

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Andy on 2018/12/1.
+//  Copyright © 2018年 wangyawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Andy on 2018/12/1.
+//  Copyright © 2018年 wangyawei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
